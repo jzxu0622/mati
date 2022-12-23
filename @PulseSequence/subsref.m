2742a1e387a22c9bebf1af5c15f29ecb36eb8227
Junzhong Xu <54155467+jzxu0622@users.noreply.github.com>
2022-12-23 16:27:28 -0600

    Update subsref.m

--- @PulseSequence/subsref.m
+++ @PulseSequence/subsref.m
@@ -21,7 +21,7 @@ switch s(1).type
                 if strcmp(fields{n}, 'gamma') 
                     tmpStruct.gamma = this.(fields{n}) ; 
                 elseif strcmp(fields{n}, 'Nacq')
-                    tmpStruct.Nacq = sum(s.subs{:}) ; 
+                    tmpStruct.Nacq = sum(s.subs{:}>0) ; % '>0' added by jzxu 20211008
                 elseif isempty(this.(fields{n}))
                     tmpStruct.(fields{n}) = [] ; 
                 else
